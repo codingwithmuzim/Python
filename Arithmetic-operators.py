45500b50139cdde043caf2307fee2a9980d153a9
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-11 13:22:04 +0530

    Update Arithmetic-operators.py

--- Arithmetic-operators.py
+++ Arithmetic-operators.py
@@ -1,1 +1,9 @@
-
+operator     name     example
+  +          add       a+b        for addition
+  -          sub       a-b        for subtract
+  *          mul       a*b        for multiply
+  /          div       a/b        for division
+  %        modulus     a%b        for reminder
+  **       exponent    a**b       for power
+  //       floor div   a//b       for use ex. print(10/3) // output 3.3333333
+                                  if use floor division  print(10//3) // output 3 floor division mai point ke bad ki value nhi deta means integer mai convert krte hai 
