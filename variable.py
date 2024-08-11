40d9da93fddbff334411b5a8735415c998909090
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-11 13:02:05 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,1 +1,10 @@
-
+# Variable is use to  store anything from simple data types like integers and strings to more complex data structures like lists and dictionaries
+# Variable name can't start with number ex. 12 // Not valid
+# Variable name start with alphabet ex. a,ax,a1,a2 like this
+# Variable ka name mai space nhi hona chahiye ex. a 1 //not valid
+# about_1 //valid
+a=10
+b=10
+print(a,b) //output 10,10
+print(id(a),id(b)) // id is inbuild function to use find the memory address. // output 1356435200,1356435200 same memory address coz value is same.
+if value is different than memory address is different.
