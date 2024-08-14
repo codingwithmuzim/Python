7ce8ae4030240a6bc542fc5815a5cf77fac8b1e1
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-14 22:59:07 +0530

    Update comparison-Ope.py

--- comparison-Ope.py
+++ comparison-Ope.py
@@ -1,1 +1,9 @@
+comparison between two value, person or condition
 
+Operator         e.g          Name
+  ==            x==y          Equal
+  !=            x!=y        Not Equal
+  >              x>y        Greater than
+  <              x<y         Less than
+  >=             x>=y      Greater than or equla to
+  <=             x<=y      Less than or equla to
