16c45a6e4f697197b559ce2ff197b3c482484c29
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-14 23:08:28 +0530

    Update Logical-Ope.py

--- Logical-Ope.py
+++ Logical-Ope.py
@@ -1,1 +1,17 @@
+And Operator:-->Return true if both statement are true
 
+1 consider as True
+0 consider as False
+
+a      b      a and b
+1      1         1
+1      0         0
+0      1         0
+0      0         0
+
+Or Operator:--->return True if at least one condition are True
+a      b      a or b
+1      1         1
+1      0         1
+0      1         1
+0      0         0
