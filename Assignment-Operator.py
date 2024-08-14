333623507b4a05c0b2eeb1c2d84b224b65b1560a
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-14 22:47:16 +0530

    Update Assignment-Operator.py

--- Assignment-Operator.py
+++ Assignment-Operator.py
@@ -1,1 +1,17 @@
+operator      e.g.      same as
+  =           x=4         x=4      assign the value in x
+  +=         x+=4        x=x+4     increment 
+  -=         x-=4        x=x-4     decrement 
 
+
+x=5
+print(x) // output 5
+
+
+x=x+5
+print(x) // output 10
+
+
+y=20
+y=y-10
+print(y) // output 10
