1d91be498e46c6b19fb90235a31bd633347c764f
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-15 23:28:36 +0530

    Update Membership-Identity-bitwiseOpe.py

--- Membership-Identity-bitwiseOpe.py
+++ Membership-Identity-bitwiseOpe.py
@@ -8,3 +8,15 @@ Operator         Description                                              Exampl
   is             return true if both variables are the same obeject       x is y         // is same as equle to
  is not          return true if both variables are the same obeject       x is not y     // is not same as not equal to
 
+Bitwise Operator
+Operator         Description          Example
+  &                AND                 x & y
+  |                OR                  x | y
+  ^                XOR                 x ^ y      only one condition true than return true, if both are true than return false, if both are false than return false
+
+  XOR
+  A        B        A ^ B
+  0        0          0       
+  1        0          1
+  0        1          1
+  1        1          0
