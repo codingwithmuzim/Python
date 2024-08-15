5d2cfab308b06acb375f6353697374da75ee8241
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-15 23:13:52 +0530

    Update Membership-Identity-bitwiseOpe.py

--- Membership-Identity-bitwiseOpe.py
+++ Membership-Identity-bitwiseOpe.py
@@ -1,3 +1,10 @@
+Membership Operator
 Operator         Description                                      Example
   in             return true if the value is present              x in y 
 not in           return false if the value is not present        x not in y
+
+Identity Operator
+Operator         Description                                              Example
+  is             return true if both variables are the same obeject       x is y         // is same as equle to
+ is not          return true if both variables are the same obeject       x is not y     // is not same as not equal to
+
