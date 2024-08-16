db4c6a5731bca8ddfad7aecd5cd1efbb60ee27ea
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-16 23:43:46 +0530

    Update Membership-Identity-bitwiseOpe.py

--- Membership-Identity-bitwiseOpe.py
+++ Membership-Identity-bitwiseOpe.py
@@ -1,7 +1,7 @@
 Membership Operator
 Operator         Description                                      Example
-  in             return true if the value is present              x in y 
-not in           return false if the value is not present        x not in y
+  in             return true if the value is present               x in y 
+not in           return false if the value is not present         x not in y
 
 Identity Operator
 Operator         Description                                              Example
