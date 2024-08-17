e6620442553e8cf98103cec9009c0960586042f2
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-17 23:26:00 +0530

    Update User-input-type-casting.py

--- User-input-type-casting.py
+++ User-input-type-casting.py
@@ -3,3 +3,7 @@
 ---> Floot() // flooting point number
 ---> eval()  // all consider ex. int, floot,binary type value 
 
+
+a= int(input("enter the value 1"))   //  integer 1,2,4 
+a= floot(input("enter the value 1")) //  floot 2.3221, 1.32
+
