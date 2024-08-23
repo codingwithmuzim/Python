593ad63e615174292df4a88ad136d5a40bdc02b8
Muzim <158441313+codingwithmuzim@users.noreply.github.com>
2024-08-23 23:36:05 +0530

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,18 +1,33 @@
-# Python is a high-level, interpreted programming language known for its simplicity and readability. 
+# Python is a high-level, interpreted programming language known for its simplicity and readability.
+
 # It was created by Guido van Rossum and first released in 1991. 
-# Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. 
+
+# Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming.
+
 # It's widely used for web development, data analysis, artificial intelligence, scientific computing, automation, and more.
+
 
 # Key features of Python include:
 
+
 # Readable and Clean Syntax:->
+
 #  Python's syntax is clear and easy to understand, making it ideal for beginners and experienced programmers alike.
+
 # Interpreted Language:->
+
 # Python code is executed line by line, which allows for rapid prototyping and testing.
+
 # Dynamic Typing:->
+
 # Python uses dynamic typing, meaning you don't have to declare variable types explicitly.
+
 # Extensive Standard Library: ->
+
 # Python has a rich standard library that supports a wide range of tasks, from file manipulation to network programming.
+
 # Large Community and Ecosystem:->
+
 #  Python has a vast community that contributes to a large number of libraries and frameworks, making it a versatile tool for many applications.
+
 # The simplicity and versatility of Python have made it one of the most popular programming languages in the world.
